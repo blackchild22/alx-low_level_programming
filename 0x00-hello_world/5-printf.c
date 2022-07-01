fda8847d2a8faac47d74a6fe48b0b89a0b3eb96a
blackchild22 <iroadioguuchechukwu@gmail.com>
2022-07-01 06:06:17 +0100

    c_program

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- *main - is void
- *return : 0
+ * main - is void
+ * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 /**
  *return : 0
  *
