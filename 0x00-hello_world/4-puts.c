19b99b08cf7a51f38cc52112a67df224bbaee392
blackchild22 <iroadioguuchechukwu@gmail.com>
2022-07-01 05:31:10 +0100

    c_program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-
+/*
+ * Entry has to be void
+ * Return is 0
+ */
 int main(void)
 {
-
-	puts("\"Programming is like building a multiling puzle");
-	
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
