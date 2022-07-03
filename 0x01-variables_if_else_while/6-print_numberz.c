14253220c6356caa355d2a29f750faff57efc7cd
blackchild22 <iroadioguuchechukwu@gmail.com>
2022-07-03 22:22:20 +0100

    c-variables

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	for (n = 0; n < 10; n++)
+	{
+		putchar('n');
+	}
+	putchar('\n');
+	return (0);
+}
