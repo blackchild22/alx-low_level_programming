1a4229888395a685aa4aab68762bf9a2fa3b422f
blackchild22 <iroadioguuchechukwu@gmail.com>
2022-07-05 22:22:36 +0100

    c-function

---
+++ 0x02-functions_nesed_loops/0-putchar.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	char a = '_';
+	char b = 'p';
+	char c = 'u';
+	char d = 't';
+	char e = 'c';
+	char f = 'h';
+	char g = 'a';
+	char h = 'r';
+
+	putchar(a);
+	putchar(b);
+	putchar(c);
+	putchar(d);
+	putchar(e);
+	putchar(f);
+	putchar(g);
+	putchar(h);
+	putchar('\n');
+	return (0);
+}
