8acc64e1894065ab9b5effdbfc6ae02207cbeb6c
blackchild22 <iroadioguuchechukwu@gmail.com>
2022-07-06 20:39:05 +0100

    c-function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,8 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
- * main - Program to output _putchar to the standard output 
-=======
  * main - Program to output _putchar to the standard output
->>>>>>> 8079a4f001ad4acf73f8b438b6fedf40ffbf3048
+ * main - Program to output _putchar to the standard output
  *
  * Return: 0
  *
