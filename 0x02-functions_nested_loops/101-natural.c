f5aa77accbe15d307278fc995d3cb2b3e6c4110a
blackchild22 <iroadioguuchechukwu@gmail.com>
2022-07-06 21:38:59 +0100

    c-function

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program to calculate sum of the multiples of 3 and 5
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int m;
+	int result;
+
+	for (m = 0; m < 1024; m++)
+	{
+		if ((m % 3 == 0) || (m % 5 == 0))
+		{
+			result = result + m;
+		}
+		else
+		{
+			continue;
+		}
+	}
+	printf("%d\n", result);
+	return (0);
+}
