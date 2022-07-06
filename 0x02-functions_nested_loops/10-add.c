31e1a38e1cf86efdd63c744c6c5408c3bbf19406
blackchild22 <iroadioguuchechukwu@gmail.com>
2022-07-06 21:33:06 +0100

    c-function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Program to add two numbers
+ *
+ * @n: integer input
+ *
+ * @j: second integer input
+ *
+ * Return: returns sum of the numbers
+ */
+int add(int n, int j)
+{
+	return (n + j);
+}
