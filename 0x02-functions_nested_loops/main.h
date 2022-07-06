058b52bafb11bc0f21c8d86c33010620cac5cba7
blackchild22 <iroadioguuchechukwu@gmail.com>
2022-07-06 20:42:12 +0100

    c-function

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,12 @@
-/** header file to store all my prototype functions */
-
-char _putchar(char c);
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int n);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
