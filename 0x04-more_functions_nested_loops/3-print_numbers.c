cccb3a608a82fa7b7bffab47968a5f0c4c287347
blackchild22 <iroadioguuchechukwu@gmail.com>
2022-07-08 06:36:36 +0100

    c-functions2

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,20 @@
 #include "main.h"
-
 /**
- * print_numbers - Program to print the digits 1 to 9 followed by new line
+ *print_most_numbers - prints most except 2 and 4.
  *
- * Return: void
+ *Return: void.
  */
-
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		_putchar(i + '0');
+		if ((i == 2) || (i == 4))
+		{
+			continue;
+		}
+		_putchar('0' + i);
 	}
 	_putchar('\n');
 }
