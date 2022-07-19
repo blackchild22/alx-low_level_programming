e516c51eaeec35fe9ce7b8d13570ce8480e42260
blackchild22 <iroadioguuchechukwu@gmail.com>
2022-07-19 21:43:58 +0100

    hello

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _memset - program to fill a memory area with a constant byte
+ *
+ * @s: string input
+ *
+ * @b: constant byte
+ *
+ * @n: limit
+ *
+ * Return: char
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+        unsigned int i;
+
+        for (i = 0; i < n; i++)
+        {
+                *(s + i) = b;
+        }
+        return (s);
+}
