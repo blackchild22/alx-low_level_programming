6b627c0605a94ac2dfb461142098aa2632d4aa5b
blackchild22 <iroadioguuchechukwu@gmail.com>
2022-07-19 22:06:00 +0100

    hello

--- 0x07-pointers_arrays_strings/0-memcpy.c
+++ 0x07-pointers_arrays_strings/0-memcpy.c
@@ -1,24 +1,24 @@
-#include "main.h"
+#include "main.h"
 
 /**
- * _memcpy - program to copy n part of the memory area of a string to another 
- *
- * @dest: destination string
- *
- * @src: source string
- *
- * @n: limit
- *
- * Return: pointer to dest
- */
+ * _memcpy - program to copy n part of the memory area of a string to another
+ *
+ * @dest: destination string
+ *
+ * @src: source string
+ *
+ * @n: limit
+ *
+ * Return: pointer to dest
+ */
 
-char *_memcpy(char *dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-        unsigned int i;
+	unsigned int i;
 
-        for (i = 0; i < n; i++)
-        {
-                *(dest + i) = *(src + i);
-        }
-        return (dest);
+	for (i = 0; i < n; i++)
+	{
+		 *(dest + i) = *(src + i);
+	}
+	return (dest);
 }
